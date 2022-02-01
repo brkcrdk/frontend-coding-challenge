69080b5b89d36ed312cf643ddf52667267768248
Burak <burak@90pixel.com>
2022-02-01 18:31:29 +0300

    Add debouncer hook.

--- src/pages/MainPage/Header.tsx
+++ src/pages/MainPage/Header.tsx
@@ -1,16 +1,18 @@
 import React, { useEffect, useState } from 'react';
 import { useDispatch } from 'react-redux';
 import styled from 'styled-components';
+import { useDebounce } from 'usehooks-ts';
 import { Button, Input } from '../../components';
 import { updateFilter } from '../../store/actions/filter';
 
 const Header = () => {
   const dispatch = useDispatch();
   const [searchValue, setValue] = useState('');
+  const debouncedValue = useDebounce<string>(searchValue, 500);
 
   useEffect(() => {
-    dispatch(updateFilter(searchValue));
-  }, [dispatch, searchValue]);
+    dispatch(updateFilter(debouncedValue));
+  }, [dispatch, debouncedValue]);
 
   return (
     <HeaderContainer>
