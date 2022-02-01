473be539503c3f5d91dd8bc4bc04b2be9c8cbd94
Burak <burak@90pixel.com>
2022-02-01 16:08:20 +0300

    Add filter functionality to Header.

--- src/pages/MainPage/Header.tsx
+++ src/pages/MainPage/Header.tsx
@@ -1,11 +1,24 @@
-import React from 'react';
+import React, { useEffect, useState } from 'react';
+import { useDispatch } from 'react-redux';
 import styled from 'styled-components';
 import { Button, Input } from '../../components';
+import { updateFilter } from '../../store/actions/filter';
 
 const Header = () => {
+  const dispatch = useDispatch();
+  const [searchValue, setValue] = useState('');
+
+  useEffect(() => {
+    dispatch(updateFilter(searchValue));
+  }, [dispatch, searchValue]);
+
   return (
     <HeaderContainer>
-      <Input placeholder="Search tournament..." />
+      <Input
+        placeholder="Search tournament..."
+        value={searchValue}
+        onChange={e => setValue(e.target.value)}
+      />
       <Button>CREATE TOURNAMENT</Button>
     </HeaderContainer>
   );
