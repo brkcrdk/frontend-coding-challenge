004c5d82107659fcd035a5e3161bc39d04912861
Burak <burak@90pixel.com>
2022-02-02 14:26:29 +0300

    Add adding new tournament functionaltiy.

--- src/pages/MainPage/Header.tsx
+++ src/pages/MainPage/Header.tsx
@@ -4,15 +4,25 @@ import styled from 'styled-components';
 import { useDebounce } from 'usehooks-ts';
 import { Button, Input } from '../../components';
 import { updateFilter } from '../../store/actions/filter';
+import { addNewTournament } from '../../store/actions/tournaments';
+import { createTournament } from '../../requests/createTournament';
 
 const Header = () => {
   const dispatch = useDispatch();
   const [searchValue, setValue] = useState('');
-  const debouncedValue = useDebounce<string>(searchValue, 500);
+  const debouncedValue = useDebounce(searchValue, 500);
 
   useEffect(() => {
     dispatch(updateFilter(debouncedValue));
   }, [dispatch, debouncedValue]);
+
+  const handleAddTournament = async () => {
+    const tournamentName = prompt('New Tournament Name:');
+    if (tournamentName) {
+      const newTournament = await createTournament(tournamentName);
+      dispatch(addNewTournament(newTournament));
+    }
+  };
 
   return (
     <HeaderContainer>
@@ -21,7 +31,7 @@ const Header = () => {
         value={searchValue}
         onChange={e => setValue(e.target.value)}
       />
-      <Button>CREATE TOURNAMENT</Button>
+      <Button onClick={handleAddTournament}>CREATE TOURNAMENT</Button>
     </HeaderContainer>
   );
 };
