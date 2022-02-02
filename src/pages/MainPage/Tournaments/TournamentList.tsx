b13fac9e04140cb2be1d5b3a20bed23f03d488b0
Burak <burak@90pixel.com>
2022-02-02 14:55:48 +0300

    Add responsive layout for tournaments list.

--- src/pages/MainPage/Tournaments/TournamentList.tsx
+++ src/pages/MainPage/Tournaments/TournamentList.tsx
@@ -43,6 +43,12 @@ const ListContainer = styled.div`
 
 const CardList = styled.div`
   display: grid;
-  grid-template-columns: repeat(auto-fit, minmax(250px, 1fr));
+  grid-template-columns: repeat(3, 1fr);
   gap: ${theme.spacing(6)};
+  @media (max-width: 768px) {
+    grid-template-columns: repeat(2, 1fr);
+  }
+  @media (max-width: 500px) {
+    grid-template-columns: 1fr;
+  }
 `;
