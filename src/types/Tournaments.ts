c53f24a213ad8b990e72870d34ffe013efa3ca17
Burak <burak@90pixel.com>
2022-02-03 15:24:27 +0300

    Refactor.

--- src/types/Tournaments.ts
+++ src/types/Tournaments.ts
@@ -1,15 +1,8 @@
-type GameTypes =
-  | 'Counter-Strike: Global Offensive'
-  | 'Dota 2'
-  | 'Rocket League'
-  | 'Battalion 1944'
-  | 'League of Legends';
-
 export interface TournamentProps {
   id: string;
   name: string;
   organizer: string;
-  game: GameTypes;
+  game: string;
   participants: {
     current: number;
     max: number;
